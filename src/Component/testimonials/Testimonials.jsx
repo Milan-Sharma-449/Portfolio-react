6137254e68ffcce0c1fe28b8299ba2118f30868d
Milan-Sharma-449 <milansharma449@gmail.com>
2022-12-13 22:42:15 +0530

    designed testimonials

--- src/Component/testimonials/Testimonials.jsx
+++ src/Component/testimonials/Testimonials.jsx
@@ -7,6 +7,18 @@ const data = [
     title: 'Anubhav Dutta',
     designation: 'SDE-3 HighRadius Corporate',
     quote: 'Milan is a great co-worker and problem solver. He is quick to extend his helping hand and makes a good team player.'
+  },
+  {
+    id: 2,
+    title: 'Anubhav Dutta',
+    designation: 'SDE-3 HighRadius Corporate',
+    quote: 'Milan is a great co-worker and problem solver. He is quick to extend his helping hand and makes a good team player.'
+  },
+  {
+    id: 3,
+    title: 'Anubhav Dutta',
+    designation: 'SDE-3 HighRadius Corporate',
+    quote: 'Milan is a great co-worker and problem solver. He is quick to extend his helping hand and makes a good team player.'
   }
 ]
 
@@ -16,13 +28,13 @@ const Testimonials = () => {
       <h5>Reviews</h5>
       <h2>Testimonials</h2>
 
-      <div className='.container testimonials__container' >
+      <div className='container testimonials__container' >
       {
       data.map(({id, title, designation, quote}) => {
         return (
           <article key={id} className='testimonial'>
-          <h4>{title}</h4>
-          <h6>{designation}</h6>
+          <h3 className='title' >{title}</h3>
+          <h5 className='desig' >{designation}</h5>
           <small className='review'>{quote}</small>
         </article>
         )
