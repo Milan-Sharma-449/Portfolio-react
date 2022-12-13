1841104825e01ae8f459ba8bacbfc1780a445623
Milan-Sharma-449 <milansharma449@gmail.com>
2022-12-13 22:27:42 +0530

    added testimonials

--- src/Component/testimonials/Testimonials.jsx
+++ src/Component/testimonials/Testimonials.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './testimonials.css'
 
+const data = [
+  {
+    id: 1,
+    title: 'Anubhav Dutta',
+    designation: 'SDE-3 HighRadius Corporate',
+    quote: 'Milan is a great co-worker and problem solver. He is quick to extend his helping hand and makes a good team player.'
+  }
+]
+
 const Testimonials = () => {
   return (
     <section id='testimonials' >
-      Testimonials
+      <h5>Reviews</h5>
+      <h2>Testimonials</h2>
+
+      <div className='.container testimonials__container' >
+      {
+      data.map(({id, title, designation, quote}) => {
+        return (
+          <article key={id} className='testimonial'>
+          <h4>{title}</h4>
+          <h6>{designation}</h6>
+          <small className='review'>{quote}</small>
+        </article>
+        )
+      })
+    }
+      </div>
     </section>
   )
 }
